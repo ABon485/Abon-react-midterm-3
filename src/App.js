5fb28389f000e4f40e23efe7688f4987dbc9416c
bon bon <bona35165@gmail.com>
2024-06-08 21:52:05 +0700

    update App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useEffect } from "react";
+import "./App.css";
+import Home from "./components/layout/Home";
+import Navbar from "./components/layout/Navbar";
+import { useStore } from "./components/store";
+const App = () => {
+  const [state] = useStore();
 
-function App() {
+  useEffect(() => {
+    document.body.className = state.mode === "light" ? "bg-light" : "bg-dark";
+  }, [state.mode]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Home />
+      </Router>
     </div>
   );
-}
-
+};
 export default App;
